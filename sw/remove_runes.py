89cf81c60fd92f2aa20c5f1d9889e609acc3c731
Alexandre Zhan <azhan1996@gmail.com>
2021-07-12 01:26:35 +0200

    sw: finish remove_rune

--- sw/remove_runes.py
+++ sw/remove_runes.py
@@ -3,11 +3,18 @@ import os
 
 path = "C:/Users/Alexandre/AppData/Local/S2US/Screenshots/Runes/SOLD/"
 count = 0
+useless = {"[ATK]", "[HP]", "[DEF]"}
 
 with os.scandir(path) as runes:
 	for rune in runes:
-		if rune.name
+		if not rune.is_file():
+			continue
+		lr = rune.name.split(" ")
+		if int(lr[5][2]) % 2 == 0:
+			if lr[6] in useless:
+				print(rune.name)
+				count += 1
+				os.remove(path + rune.name)
+	print(count)
 
 
-
-print(len(runes))
