342af33fa4f5b74236d95ee95a23f4e00f953a94
Alexandre Zhan <azhan1996@gmail.com>
2021-07-12 02:57:17 +0200

    sw: add stats

---
+++ sw/stats.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+
+path = "C:/Users/Alexandre/AppData/Local/S2US/Screenshots/Runes/"
+status = ["GOT", "SOLD"]
+
+for s in status:
+	count = 0
+	check = True
+	with os.scandir(path + s) as runes:
+		print(s + " runes:")
+		for rune in runes:
+			check = True
+			if not rune.is_file():
+				continue
+			for i in range(1, len(sys.argv)):
+				if sys.argv[i] not in rune.name:
+					check &= False
+			if check:
+				print(rune.name)
+				count += 1
+	print (count)
